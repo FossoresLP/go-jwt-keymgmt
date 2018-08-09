1139fda145660104b9f2c8c96d2aa0020f98cfb5
Pascal Vorwerk <info@fossores.de>
2018-08-09 17:00:06 +0200

    Make generator assignable and therefore self  contained Moving generator into a type to make it assignable to variables. Instances are still not allowed as they would fail due to jwt not being self contained but storing keys package wide.

--- generator/generator.go
+++ generator/generator.go
@@ -12,21 +12,24 @@ import (
 	"github.com/fossoreslp/go-uuid-v4"
 )
 
-var privateKey ed25519.PrivateKey
-var publicKey ed25519.PublicKey
-var keyID uuid.UUID
-var keyServerURL string
+// Signer is the type used to store the JWT generator
+type Signer struct {
+	privateKey   ed25519.PrivateKey
+	publicKey    ed25519.PublicKey
+	keyID        uuid.UUID
+	keyServerURL string
+}
 
-// NewKeySet takes a keyserver URL and generates a new set of public and private keys.
+// NewKeySet takes a keyserver URL and creates a Signer with a new set of public and private keys.
 // It also submits the public key to the keyserver.
-func NewKeySet(keyServer string) (err error) {
-	keyServerURL = keyServer
-	publicKey, privateKey, err = ed25519.GenerateKey(nil)
+func NewKeySet(keyServerURL string) (sig *Signer, err error) {
+	sig.keyServerURL = keyServerURL
+	sig.publicKey, sig.privateKey, err = ed25519.GenerateKey(nil)
 	if err != nil {
 		return
 	}
-	pubReader := bytes.NewReader(publicKey)
-	resp, err := http.Post(keyServerURL, "application/octet-stream", pubReader)
+	pubReader := bytes.NewReader(sig.publicKey)
+	resp, err := http.Post(sig.keyServerURL, "application/octet-stream", pubReader)
 	if err != nil {
 		return
 	}
@@ -36,37 +39,37 @@ func NewKeySet(keyServer string) (err error) {
 		return
 	}
 	if l != 36 {
-		return errors.New("key ID has wrong length")
+		return nil, errors.New("key ID has wrong length")
 	}
-	keyID, err = uuid.Parse(string(rawBody))
+	sig.keyID, err = uuid.Parse(string(rawBody))
 	if err != nil {
 		return
 	}
-	jwt.Setup(privateKey)
-	return nil
+	jwt.Setup(sig.privateKey)
+	return nil, nil
 }
 
 // NewJWT takes the desired content as an argument and generates a JWT with key ID and keyserver URL set by this package.
 // It may return an error in case it is not possible to generate a JWT for some reason.
-func NewJWT(content interface{}) (jwt.JWT, error) {
-	return jwt.NewWithKeyIDAndKeyURL(content, keyID.String(), keyServerURL)
+func (sig *Signer) NewJWT(content interface{}) (jwt.JWT, error) {
+	return jwt.NewWithKeyIDAndKeyURL(content, sig.keyID.String(), sig.keyServerURL)
 }
 
 // SafeExit should be deferred when calling NewKeySet as it securely erases the keys from memory and hopefully also from swap files in case they were paged out.
 // It currently overwrites the keys with random data (or zero data in case a crypto/rand fails) 10 times. This might be overkill but it only runs once so there shouldn't be any issues.
-func SafeExit() {
+func (sig *Signer) SafeExit() {
 	for i := 0; i < 10; i++ {
-		_, err := rand.Read(publicKey)
+		_, err := rand.Read(sig.publicKey)
 		if err != nil {
-			publicKey = []byte{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0}
+			sig.publicKey = []byte{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0}
 		}
 
-		_, err = rand.Read(privateKey)
+		_, err = rand.Read(sig.privateKey)
 		if err != nil {
-			privateKey = []byte{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0}
+			sig.privateKey = []byte{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0}
 		}
 	}
-	if publicKey == nil || privateKey == nil {
+	if sig.publicKey == nil || sig.privateKey == nil {
 		panic("something failed")
 	}
 }
